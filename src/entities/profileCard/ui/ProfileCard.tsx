825d45ac31260ed7ac33e71bb3884de29c8bf0b7
quthery <chiefkeef7317@gmail.com>
2025-08-22 20:00:39 +0300

    feat: добавил карточку профиля на sidebar

--- src/entities/profileCard/ui/ProfileCard.tsx
+++ src/entities/profileCard/ui/ProfileCard.tsx
@@ -1,8 +1,21 @@
+import s from './ProfileCard.module.scss';
+import img from '/avatar.jpg'
+
+
 export function ProfileCard() {
     return (
-        <div className="profile-card">
-            <h2>Profile Card</h2>
-            <p>This is a profile card component.</p>
+        <div className={s['profileCard']}>
+            <div className={s['profile-header']}>
+                <div className={s['img-container']}>
+                    <img className={s['profile-img']} src={img} alt="profile-img" />
+                    <div className={s['online-dot']} aria-label='online'></div>
+                </div>
+                <div className={s['profile-name-container']}>
+                    <h2 className={s['profile-name-text']}>Valery Ivanov</h2>
+                    <p className={s['profile-username']}>@mamkincoder227</p>
+                    
+                </div> 
+            </div>
         </div>
     );
 }   
--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -1,8 +1,10 @@
 import cl from "./SideBar.module.scss";
+import  {ProfileCard}  from "@/entities/profileCard";
 
 export function Sidebar() {
   return (
     <div className={cl["sidenav"]}>
+      <ProfileCard />
       <a href="#">About</a>
       <a href="#">Services</a>
       <a href="#">Clients</a>
